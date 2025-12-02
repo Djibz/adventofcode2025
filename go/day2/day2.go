7ce05f47603b1c8291fbbf86c7424b423013731e
Erwan <erwan.brunelliere@gmail.com>
2025-12-03 00:03:52 +0100

    day 2

---
+++ go/day2/day2.go
@@ -0,0 +1,94 @@
+package day2
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func commaSplit(data []byte, atEOF bool) (advance int, token []byte, err error) {
+
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+
+	if i := strings.Index(string(data), ","); i >= 0 {
+		return i + 1, data[0:i], nil
+	}
+
+	if i := strings.Index(string(data), "\n"); i >= 0 {
+		return i + 1, data[0:i], nil
+	}
+
+	if atEOF {
+		return len(data), data, nil
+	}
+
+	return
+}
+
+func First(file *os.File) int {
+	counter := 0
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(commaSplit)
+	for scanner.Scan() {
+		values := strings.Split(scanner.Text(), "-")
+		first, _ := strconv.Atoi(values[0])
+		second, _ := strconv.Atoi(values[1])
+
+		for first <= second {
+			str := strconv.Itoa(first)
+			l := len(str)
+			if l%2 == 0 {
+				if str[0:l/2] == str[l/2:] {
+					counter += first
+				}
+			}
+
+			first++
+		}
+	}
+
+	return counter
+}
+
+func Second(file *os.File) int {
+	counter := 0
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(commaSplit)
+	for scanner.Scan() {
+		values := strings.Split(scanner.Text(), "-")
+		first, _ := strconv.Atoi(values[0])
+		second, _ := strconv.Atoi(values[1])
+
+		already := []int{}
+
+		for first <= second {
+			str := strconv.Itoa(first)
+
+			l := 1
+			for l <= len(str)/2 {
+				r, _ := regexp.Compile(fmt.Sprintf(`^(%s)+$`, str[:l]))
+				result := r.MatchString(str)
+
+				if result && !slices.Contains(already, first) {
+					// fmt.Println(first)
+					already = append(already, first)
+					counter += first
+				}
+
+				l++
+			}
+
+			first++
+		}
+	}
+
+	return counter
+}
--- go/main.go
+++ go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"aoc/day1"
+	"aoc/day2"
 	"fmt"
 	"os"
 )
@@ -18,4 +19,11 @@ func main() {
 	fmt.Println("Day 1 step 1:", day1.First(file))
 	file.Seek(0, 0)
 	fmt.Println("Day 1 step 2:", day1.Second(file))
+	file.Close()
+
+	file, err = os.Open("../resources/2/input.txt")
+	check(err)
+	fmt.Println("Day 2 step 1:", day2.First(file))
+	file.Seek(0, 0)
+	fmt.Println("Day 2 step 2:", day2.Second(file))
 }
