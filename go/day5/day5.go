ac20878e5204f39bc5ab8a09925bcb70bf99d4ad
Erwan <erwan.brunelliere@gmail.com>
2025-12-11 20:46:12 +0100

    JOUR 5 ;-;

--- go/day5/day5.go
+++ go/day5/day5.go
@@ -50,8 +50,6 @@ func First(file *os.File) int {
 	}
 
 	slices.Sort(ingredients)
-	fmt.Println(ranges)
-	fmt.Println(ingredients)
 
 	total := 0
 	for _, r := range ranges {
@@ -65,7 +63,7 @@ func First(file *os.File) int {
 }
 
 func Second(file *os.File) int {
-	ranges := [][]int{}
+	ranges := []int{}
 
 	for line := range tools.LinesChan(file) {
 		if line == "" {
@@ -76,12 +74,78 @@ func Second(file *os.File) int {
 		from, _ := strconv.Atoi(values[0])
 		to, _ := strconv.Atoi(values[1])
 
-		ranges = append(ranges, []int{from, to})
+		// fmt.Println(ranges)
+		// fmt.Println(from, to)
+		ranges = addDomain(ranges, from, to)
+		// fmt.Println(ranges)
+		// fmt.Println()
 	}
 
-	return 0
+	total := 0
+	for i := range ranges {
+		if i%2 != 0 {
+			total += ranges[i] - ranges[i-1] + 1
+		}
+	}
+
+	return total
 }
 
-func addDomain(domains [][]int, singleton []int) [][]int {
+func addDomain(domains []int, from int, to int) []int {
+	i1 := placeToBe(domains, from)
+	i2 := placeToBe(domains, to)
+	l := len(domains)
+	fmt.Println(i1, i2)
 
+	i2_even := i2%2 == 0
+	i2_equal := i2 < l && to == domains[i2]
+
+	if i1 < l && from == domains[i1] {
+		if i1%2 == 0 {
+			// in valid -> nothing to do
+		} else {
+			// use previous start
+			i1--
+		}
+	} else {
+		if i1%2 == 0 {
+			// in invalid -> create new
+			domains = slices.Insert(domains, i1, from)
+			// push everything
+			i2++
+		} else {
+			// in valid -> use previous
+			i1--
+		}
+	}
+
+	if i2_equal {
+		if i2_even {
+			// start of range -> use end
+			i2++
+		} else {
+			// end of range -> use it
+		}
+	} else {
+		if i2_even {
+			// insert new
+			domains = slices.Insert(domains, i2, to)
+		} else {
+			// in valid-> use it
+		}
+	}
+
+	fmt.Println(domains)
+	fmt.Println(i1, i2)
+	return slices.Delete(domains, i1+1, i2)
+}
+
+func placeToBe(list []int, value int) int {
+	for i, v := range list {
+		if v >= value {
+			return i
+		}
+	}
+
+	return len(list)
 }
